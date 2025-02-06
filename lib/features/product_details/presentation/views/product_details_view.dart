3391af1187910c2b4622caa8b3e245299be9b279
HusseinMohammed <Husseinsaqr43@gmail.com>
2025-02-06 08:53:06 +0200

    Create BlocProvider for favorites

--- lib/features/product_details/presentation/views/product_details_view.dart
+++ lib/features/product_details/presentation/views/product_details_view.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce_task/core/utils/custom_widgets/base_scaffold.dart';
 import 'package:e_commerce_task/core/utils/custom_widgets/responsive_layout.dart';
+import 'package:e_commerce_task/features/favorites/presentation/cubit/local_favorite_cubit.dart';
 import 'package:e_commerce_task/features/home/data/models/product_model.dart';
-import 'package:e_commerce_task/features/product_details/presentation/widgets/product_details_desktop_widget.dart';
-import 'package:e_commerce_task/features/product_details/presentation/widgets/product_details_mobile_widget.dart';
-import 'package:e_commerce_task/features/product_details/presentation/widgets/product_details_tablet_widget.dart';
+import 'package:e_commerce_task/features/product_details/presentation/widgets/custom_details_small_widget.dart';
+import 'package:e_commerce_task/features/product_details/presentation/widgets/custom_details_big_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class ProductDetailsView extends StatelessWidget {
   final ProductModel product;
@@ -18,15 +20,18 @@ class ProductDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BaseScaffold(
       title: product.title,
-      body: ResponsiveLayout(
-        mobileView: ProductDetailsMobileWidget(
-          product: product,
-        ),
-        tabletView: ProductDetailsTabletWidget(
-          product: product,
-        ),
-        desktopView: ProductDetailsDesktopWidget(
-          product: product,
+      body: BlocProvider(
+        create: (_) => LocalFavoritesCubit(),
+        child: ResponsiveLayout(
+          mobileView: CustomDetailsSmallWidget(
+            product: product,
+          ),
+          tabletView: CustomDetailsBigWidget(
+            product: product,
+          ),
+          desktopView: CustomDetailsBigWidget(
+            product: product,
+          ),
         ),
       ),
     );
