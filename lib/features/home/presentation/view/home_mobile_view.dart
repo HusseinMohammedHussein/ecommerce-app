329f6a993779898798d96011fd32f04f64d3e54c
HusseinMohammed <Husseinsaqr43@gmail.com>
2025-02-06 08:52:30 +0200

    Clean Code

--- lib/features/home/presentation/view/home_mobile_view.dart
+++ lib/features/home/presentation/view/home_mobile_view.dart
@@ -1,10 +1,11 @@
+
 import 'package:e_commerce_task/core/utils/custom_widgets/no_data_widget.dart';
-import 'package:e_commerce_task/core/utils/custom_widgets/base_scaffold.dart';
-import 'package:e_commerce_task/features/home/presentation/cubit/product_state.dart';
+import 'package:e_commerce_task/features/home/presentation/cubit/products_state.dart';
 import 'package:e_commerce_task/features/home/presentation/cubit/products_cubit.dart';
 import 'package:e_commerce_task/features/home/presentation/widgets/product_item_mobile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class HomeMobileView extends StatelessWidget {
   const HomeMobileView({super.key});
@@ -14,9 +15,9 @@ class HomeMobileView extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        BlocBuilder<ProductCubit, ProductState>(
+        BlocBuilder<ProductsCubit, ProductsState>(
           builder: (ctx, state) {
-            if (state is ProductSuccessState) {
+            if (state is ProductsSuccessState) {
               return Flexible(
                 child: ListView.builder(
                   itemCount: state.productModel?.length,
@@ -29,7 +30,7 @@ class HomeMobileView extends StatelessWidget {
                 ),
               );
             }
-            if (state is ProductLoadingState) {
+            if (state is ProductsLoadingState) {
               return const Center(
                 child: CircularProgressIndicator(),
               );
