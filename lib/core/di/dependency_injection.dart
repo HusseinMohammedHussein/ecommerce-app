b1702bc8faf885c0c5547aed00df72fd57ac694c
HusseinMohammed <Husseinsaqr43@gmail.com>
2025-02-07 21:38:17 +0200

    Add CacheHelper for manage SharedPreference.

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,4 +1,7 @@
-import 'package:e_commerce_task/core/network/dio_consumer.dart';
+import 'package:e_commerce_task/core/database/dio_consumer.dart';
+import 'package:e_commerce_task/core/database/local/cache_helper.dart';
+// import 'package:e_commerce_task/features/favorites/data/repositories/local_favorites_impl.dart';
+// import 'package:e_commerce_task/features/favorites/presentation/cubit/local_favorite_cubit.dart';
 import 'package:e_commerce_task/features/home/data/repositories/product_repo_impl.dart';
 import 'package:e_commerce_task/features/home/presentation/cubit/products_cubit.dart';
 import 'package:get_it/get_it.dart';
@@ -6,6 +9,8 @@ import 'package:get_it/get_it.dart';
 final sl = GetIt.instance;
 
 void setupDependencyInjection() {
+  sl.registerSingleton<CacheHelper>(CacheHelper());
+  sl<CacheHelper>().init();
   sl.registerSingleton<DioConsumer>(DioConsumer());
 
   sl.registerSingleton<ProductRepoImpl>(ProductRepoImpl(sl()));
