0380c5b8d6571ea1d5e9b5eb9e039379bb730495
HusseinMohammed <Husseinsaqr43@gmail.com>
2025-02-07 21:51:45 +0200

    added favorites title

--- lib/features/favorites/presentation/views/favorites_view.dart
+++ lib/features/favorites/presentation/views/favorites_view.dart
@@ -14,6 +14,7 @@ class FavoritesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BaseScaffold(
+      title: 'My Favorites',
         body: BlocProvider(
       create: (_) => sl<LocalFavoritesCubit>(),
       child: const ResponsiveLayout(
